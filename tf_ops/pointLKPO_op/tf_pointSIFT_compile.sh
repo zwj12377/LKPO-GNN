d38f50259f21b50e64c0f17cd7ba88544b2a242f
zwj12377 <397012758@qq.com>
2020-04-07 01:29:45 +0800

    Delete tf_pointSIFT_compile.sh

--- tf_ops/pointLKPO_op/tf_pointSIFT_compile.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-/usr/local/cuda-8.0/bin/nvcc pointLKPO.cu -o pointLKPO_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-
-# TF1.2
-g++ -std=c++11 main.cpp pointLKPO_g.cu.o -o tf_pointLKPO_so.so -shared -fPIC -I /python2.7/site-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
