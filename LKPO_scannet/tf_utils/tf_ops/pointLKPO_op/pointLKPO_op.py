c16be93e45e9aa5bd786d3159edbab56b3f1f87d
zwj12377 <397012758@qq.com>
2020-04-07 01:38:51 +0800

    Add files via upload

---
+++ LKPO_scannet/tf_utils/tf_ops/pointLKPO_op/pointLKPO_op.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow.python.framework import ops
+import sys
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+
+pointLKPO_module = tf.load_op_library(os.path.join(BASE_DIR, 'tf_pointLKPO_so.so'))
+
+def pointLKPO_select(xyz, radius):
+    idx = pointLKPO_module.lkpo_select(xyz, radius)
+    return idx
+ops.NoGradient('LKPOSelect')
+
+def pointLKPO_select_four(xyz, radius):
+    idx = pointLKPO_module.lkpo_select_four(xyz, radius)
+    return idx
+ops.NoGradient('LKPOSelectFour')
+
