1a60942c519fa11d9ec6f79092167700b8ea6b1f
zwj12377 <397012758@qq.com>
2020-04-07 00:22:09 +0800

    Update tf_pointSIFT_compile.sh

--- tf_ops/pointLKPO_op/tf_pointSIFT_compile.sh
+++ tf_ops/pointLKPO_op/tf_pointSIFT_compile.sh
@@ -2,4 +2,4 @@
 /usr/local/cuda-8.0/bin/nvcc pointLKPO.cu -o pointLKPO_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 # TF1.2
-g++ -std=c++11 main.cpp pointLKPO_g.cu.o -o tf_pointLKPO_so.so -shared -fPIC -I /home/zwj/anaconda2/envs/tensorflow/lib/python2.7/site-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 main.cpp pointLKPO_g.cu.o -o tf_pointLKPO_so.so -shared -fPIC -I /python2.7/site-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
